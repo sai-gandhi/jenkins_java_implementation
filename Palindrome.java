1cae2974bd9ec14bb0a78ca5ff5900d1e4a180c5
Uppalapati Sai Gandhi <149860264+sai-gandhi@users.noreply.github.com>
2025-04-19 12:24:22 +0530

    Delete Palindrome.java

--- Palindrome.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-
-public class Palindrome {
-    public static void main(String[] args) {
-        // Replace Scanner with hardcoded input for testing
-            System.out.println("Aak Pak karepaaak\nnaaku em raaadu ra babuuuu!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-    }
-}
