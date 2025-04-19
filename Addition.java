aead59ff3397cccab147ad7a153f21b20ab3c270
Uppalapati Sai Gandhi <149860264+sai-gandhi@users.noreply.github.com>
2025-04-19 12:25:23 +0530

    Create Addition.java

---
+++ Addition.java
@@ -0,0 +1,9 @@
+public class Addition {
+    public static void main(String[] args) {
+        int a = 10; // First number
+        int b = 20; // Second number
+        int sum = a + b; // Perform addition
+
+        System.out.println("The sum is: " + sum);
+    }
+}
